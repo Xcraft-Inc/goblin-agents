d08467cf7801cf50d6db483f2259b7587edf60ac
Samuel Loup <samuel.loup@gmail.com>
2025-03-20 11:07:39 +0100

    rework, resplit responsabilities

--- yetiAgentsManager.js
+++
@@ -1,7 +0,0 @@
-const {Elf} = require('xcraft-core-goblin');
-const {
-  YetiAgentsManager,
-  YetiAgentsManagerLogic,
-} = require('goblin-agents/lib/llm/yetiAgentsManager.js');
-
-exports.xcraftCommands = Elf.birth(YetiAgentsManager, YetiAgentsManagerLogic);
