b4e0ba2464ecbac33a588536ed4dc26daba2fd00
Samuel Loup <loup@epsitec.ch>
2025-09-16 13:48:50 +0200

    simplify configs

--- config.js
+++ config.js
@@ -7,28 +7,4 @@ module.exports = [
     message: 'Agents version',
     default: 2,
   },
-  {
-    type: 'input',
-    name: 'defaultProfile',
-    message: 'Agents default profile',
-    default: null,
-  },
-  {
-    type: 'input',
-    name: 'defaultSettings',
-    message: 'Agents default setting',
-    default: null,
-  },
-  {
-    type: 'input',
-    name: 'profiles',
-    message: 'Agents profiles for overriding settings',
-    default: {},
-  },
-  {
-    type: 'input',
-    name: 'settings',
-    message: 'Settings by name',
-    default: {},
-  },
 ];
